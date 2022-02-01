1e20ebc12e34fa5b1ac9ee06d82fb8c4d6d5f230
nibilin33 <nibilin33@gmail.com>
2022-02-01 16:26:36 +0300

    core: update time

--- src/components/Job.vue
+++ src/components/Job.vue
@@ -8,6 +8,26 @@
           <span class="section-title-r"></span>
         </header>
         <div class="section-bd">
+          <JobItem company="蚂蚁金服（杭州）" time="2020.06.18 至今" />
+          <JobItem
+            company="厦门亿联网络技术股份有限公司"
+            time="2017.06 至 2020.2.21"
+          >
+            <li class="project-1">
+              <h3 class="project-name">
+                <a href="" target="_blank" title="">UC</a>
+              </h3>
+            </li>
+          </JobItem>
+          <!--经历:在校-->
+          <div class="item">
+            <header class="item-hd">
+              <h3 class="item-name">厦门亿联网络技术股份有限公司</h3>
+              <span class="item-time">2016.12.16 ~ 2017.04</span>
+              <a class="btn item-more" href="" title="">在校</a>
+            </header>
+            <div class="item-bd"></div>
+          </div>
         </div>
       </section>
     </div>
@@ -18,10 +38,7 @@
           <h2 class="section-title">技能</h2>
           <span class="section-title-r"></span>
         </header>
-        <div class="section-bd">
-        
-
-        </div>
+        <div class="section-bd"></div>
       </section>
       <!--奖项/证书-->
       <section class="prize">
@@ -65,7 +82,11 @@
 </template>
 
 <script>
+import JobItem from "./JobItem.vue";
 export default {
   name: "JobInfo",
+  components: {
+    JobItem,
+  },
 };
 </script>
