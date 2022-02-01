8e4f442bbebd8c18c03568f8e9bd86f91cc47e67
nibilin33 <nibilin33@gmail.com>
2022-02-01 10:30:53 +0300

    core: modify public path

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,10 @@
+const path = require("path");
+const assetsPath = function (_path) {
+    const assetsSubDirectory = 'static';
+  
+    return path.posix.join(assetsSubDirectory, _path)
+}
 module.exports = {
-    outputDir: "docs"
+    outputDir: "docs",
+    publicPath: "../"
 }
